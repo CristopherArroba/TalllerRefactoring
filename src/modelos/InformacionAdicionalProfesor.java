cc6bd0094b1c4a11766c122fd4245be6922ff176
CristopherArroba <86638941+CristopherArroba@users.noreply.github.com>
2022-08-10 23:09:43 -0500

    get/set

--- src/modelos/InformacionAdicionalProfesor.java
+++ src/modelos/InformacionAdicionalProfesor.java
@@ -1,10 +1,39 @@
 package modelos;
 
-public class InformacionAdicionalProfesor {
-    public int añosdeTrabajo;
-    public String facultad;
-    public double BonoFijo;
-    public double sueldo;
-    
-    
+private int añosdeTrabajo;
+    private String facultad;
+    private double BonoFijo;
+    private double sueldo;
+
+    public int getAñosdeTrabajo() {
+        return añosdeTrabajo;
+    }
+
+    public void setAñosdeTrabajo(int añosdeTrabajo) {
+        this.añosdeTrabajo = añosdeTrabajo;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public double getBonoFijo() {
+        return BonoFijo;
+    }
+
+    public void setBonoFijo(double BonoFijo) {
+        this.BonoFijo = BonoFijo;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
 }
